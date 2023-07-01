0a126d0ba536b607b35e7fbc173bb8da2da6c85e
Jith-2004 <tonghui.ji.2004@outlook.com>
2023-07-01 20:16:00 +0800

    two spheres

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -69,7 +69,7 @@ fn main() {
 
     let height: usize = 800;
     let width: usize = 1200;
-    let path = "output/2.3.jpg";
+    let path = "output/2.4.jpg";
     let quality = 100; // From 0 to 100, suggested value: 60
     let max_depth = 50;
     let aspect_ratio = 1.5;
@@ -90,67 +90,22 @@ fn main() {
 
     let checker_texture = CheckerTexture::new(Vec3::new(0.2, 0.3, 0.1), Vec3::new(0.9, 0.9, 0.9));
     world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, -1000.0, 0.0),
-        1000.0,
+        Vec3::new(0.0, -10.0, 0.0),
+        10.0,
         Lambertian::new(Box::new(checker_texture)),
     )));
-
+    let checker_texture = CheckerTexture::new(Vec3::new(0.2, 0.3, 0.1), Vec3::new(0.9, 0.9, 0.9));
     world.add(Box::new(Sphere::new(
-        Vec3::new(0.0, 1.0, 0.0),
-        1.0,
-        Dielectric::new(1.5),
+        Vec3::new(0.0, 10.0, 0.0),
+        10.0,
+        Lambertian::new(Box::new(checker_texture)),
     )));
-
-    world.add(Box::new(Sphere::new(
-        Vec3::new(-4.0, 1.0, 0.0),
-        1.0,
-        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.4, 0.2, 0.1)))),
-    )));
-
-    world.add(Box::new(Sphere::new(
-        Vec3::new(4.0, 1.0, 0.0),
-        1.0,
-        Metal::new(Vec3::new(0.7, 0.6, 0.5), 0.0),
-    )));
-
-    let mut rng = rand::thread_rng();
-
-    for a in -11..11 {
-        for b in -11..11 {
-            let choose_mat = rng.gen_range(0.0..1.0);
-            let center = Vec3::new(
-                a as f64 + 0.9 * rng.gen_range(0.0..1.0),
-                0.2,
-                b as f64 + 0.9 * rng.gen_range(0.0..1.0),
-            );
-            if (center - Vec3::new(4.0, 0.2, 0.0)).length() > 0.9 {
-                if choose_mat < 0.8 {
-                    let albedo = SolidColor::new(Vec3::random() * Vec3::random());
-                    let center2 = center + Vec3::new(0.0, rng.gen_range(0.0..0.5), 0.0);
-                    world.add(Box::new(MovingSphere::new(
-                        center,
-                        center2,
-                        0.0,
-                        1.0,
-                        0.2,
-                        Lambertian::new(Box::new(albedo)),
-                    )));
-                } else if choose_mat < 0.95 {
-                    let albedo = Vec3::random_(0.5, 1.0);
-                    let fuzz = rng.gen_range(0.0..0.5);
-                    world.add(Box::new(Sphere::new(center, 0.2, Metal::new(albedo, fuzz))));
-                } else {
-                    world.add(Box::new(Sphere::new(center, 0.2, Dielectric::new(1.5))));
-                }
-            }
-        }
-    }
 
     let lookfrom = Vec3::new(13.0, 2.0, 3.0);
     let lookat = Vec3::new(0.0, 0.0, 0.0);
     let vup = Vec3::new(0.0, 1.0, 0.0);
     let dist_to_focus = 10.0;
-    let aperture = 0.1;
+    let aperture = 0.0;
 
     let cam = Camera::new(
         lookfrom,
@@ -163,6 +118,8 @@ fn main() {
         0.0,
         1.0,
     );
+
+    let mut rng = rand::thread_rng();
 
     for j in (0..=height - 1).rev() {
         for i in 0..width {
