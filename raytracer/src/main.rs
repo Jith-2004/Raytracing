5091e9d5bd268c4d7746865260ef3bdca11d7650
Jith-2004 <tonghui.ji.2004@outlook.com>
2023-07-02 23:40:51 +0800

    book 2

--- raytracer/src/main.rs
+++ raytracer/src/main.rs
@@ -74,11 +74,11 @@ fn main() {
 
     println!("CI: {}", is_ci);
 
-    let height: usize = 300;
-    let width: usize = 300;
+    let height: usize = 800;
+    let width: usize = 800;
     let path = "output/2.18.jpg";
-    let quality = 60; // From 0 to 100, suggested value: 60
-    let max_depth = 30;
+    let quality = 200; // From 0 to 100, suggested value: 60
+    let max_depth = 50;
     let aspect_ratio = 1.0;
 
     // Create image data
@@ -95,116 +95,105 @@ fn main() {
 
     let background = Vec3::zero();
 
+    let mut rng = rand::thread_rng();
+
     let mut world = HittableList::new();
 
-    world.add(Box::new(YzRect::new(
-        0.0,
-        555.0,
-        0.0,
-        555.0,
-        555.0,
-        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.12, 0.45, 0.15)))),
-    )));
+    for i in 0..20 {
+        for j in 0..20 {
+            let w = 100.0;
+            let x0 = -1000.0 + i as f64 * w;
+            let z0 = -1000.0 + j as f64 * w;
+            let y0 = 0.0;
+            let x1 = x0 + w;
+            let y1 = rng.gen_range(1.0..101.0);
+            let z1 = z0 + w;
+            world.add(Box::new(Box_::new(
+                Vec3::new(x0, y0, z0),
+                Vec3::new(x1, y1, z1),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+                Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.48, 0.83, 0.53)))),
+            )));
+        }
+    }
 
-    world.add(Box::new(YzRect::new(
-        0.0,
-        555.0,
-        0.0,
-        555.0,
-        0.0,
-        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.65, 0.05, 0.05)))),
-    )));
-
+    let light = DiffuseLight::new(Box::new(SolidColor::new(Vec3::new(7.0, 7.0, 7.0))));
     world.add(Box::new(XzRect::new(
-        0.0,
-        555.0,
-        0.0,
-        555.0,
-        0.0,
-        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73)))),
+        123.0, 423.0, 147.0, 412.0, 554.0, light,
     )));
 
-    world.add(Box::new(XzRect::new(
+    let center1 = Vec3::new(400.0, 400.0, 200.0);
+    let center2 = center1 + Vec3::new(30.0, 0.0, 0.0);
+    let moving_sphere_material =
+        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.7, 0.3, 0.1))));
+    world.add(Box::new(MovingSphere::new(
+        center1,
+        center2,
         0.0,
-        555.0,
-        0.0,
-        555.0,
-        555.0,
-        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73)))),
+        1.0,
+        50.0,
+        moving_sphere_material,
     )));
 
-    world.add(Box::new(XyRect::new(
-        0.0,
-        555.0,
-        0.0,
-        555.0,
-        555.0,
-        Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73)))),
+    world.add(Box::new(Sphere::new(
+        Vec3::new(260.0, 150.0, 45.0),
+        50.0,
+        Dielectric::new(1.5),
     )));
 
-    world.add(Box::new(XzRect::new(
-        113.0,
-        443.0,
-        127.0,
-        432.0,
-        554.0,
-        DiffuseLight::new(Box::new(SolidColor::new(Vec3::new(7.0, 7.0, 7.0)))),
+    world.add(Box::new(Sphere::new(
+        Vec3::new(0.0, 150.0, 145.0),
+        50.0,
+        Metal::new(Vec3::new(0.8, 0.8, 0.9), 1.0),
     )));
 
-    let white0 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white1 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white2 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white3 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white4 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white5 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white6 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let box1 = Box_::new(
-        Vec3::new(0.0, 0.0, 0.0),
-        Vec3::new(165.0, 330.0, 165.0),
-        white0,
-        white1,
-        white2,
-        white3,
-        white4,
-        white5,
-        white6,
-    );
-    let box2 = RotateY::new(15.0, Box::new(box1));
-    let box3 = Translate::new(Vec3::new(265.0, 0.0, 295.0), Box::new(box2));
-    let t = SolidColor::new(Vec3::new(0.0, 0.0, 0.0));
+    let boundary = Sphere::new(Vec3::new(360.0, 150.0, 45.0), 70.0, Dielectric::new(1.5));
+    world.add(Box::new(boundary));
+
+    let boundary = Sphere::new(Vec3::new(360.0, 150.0, 45.0), 70.0, Dielectric::new(1.5));
+    let t = SolidColor::new(Vec3::new(0.2, 0.4, 0.9));
     let m = Isotropic::new(Box::new(t));
     world.add(Box::new(ConstantMedium::new(
-        0.01,
-        Box::new(box3),
+        0.2,
+        Box::new(boundary),
         Box::new(m),
     )));
-    let white0 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white1 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white2 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white3 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white4 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white5 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let white6 = Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73))));
-    let box1 = Box_::new(
-        Vec3::new(0.0, 0.0, 0.0),
-        Vec3::new(165.0, 165.0, 165.0),
-        white0,
-        white1,
-        white2,
-        white3,
-        white4,
-        white5,
-        white6,
-    );
-    let box2 = RotateY::new(-18.0, Box::new(box1));
-    let box3 = Translate::new(Vec3::new(130.0, 0.0, 65.0), Box::new(box2));
+
+    let boundary = Sphere::new(Vec3::new(0.0, 0.0, 0.0), 5000.0, Dielectric::new(1.5));
     let t = SolidColor::new(Vec3::new(1.0, 1.0, 1.0));
     let m = Isotropic::new(Box::new(t));
     world.add(Box::new(ConstantMedium::new(
-        0.01,
-        Box::new(box3),
+        0.00001,
+        Box::new(boundary),
         Box::new(m),
     )));
+
+    let earth_texture = ImageTexture::new(&Path::new("earth.jpg"));
+    world.add(Box::new(Sphere::new(
+        Vec3::new(400.0, 200.0, 400.0),
+        100.0,
+        Lambertian::new(Box::new(earth_texture)),
+    )));
+
+    let pertext = NoiseTexture::new(0.1);
+    world.add(Box::new(Sphere::new(
+        Vec3::new(220.0, 280.0, 300.0),
+        80.0,
+        Lambertian::new(Box::new(pertext)),
+    )));
+
+    for i in 0..1000 {
+        world.add(Box::new(Sphere::new(
+            Vec3::random_(0.0, 165.0) + Vec3::new(-100.0, 270.0, 395.0),
+            10.0,
+            Lambertian::new(Box::new(SolidColor::new(Vec3::new(0.73, 0.73, 0.73)))),
+        )))
+    }
 
     let lookfrom = Vec3::new(478.0, 278.0, -600.0);
     let lookat = Vec3::new(278.0, 278.0, 0.0);
@@ -223,8 +212,6 @@ fn main() {
         0.0,
         1.0,
     );
-
-    let mut rng = rand::thread_rng();
 
     for j in (0..=height - 1).rev() {
         for i in 0..width {
